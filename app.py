1ebb373bb38e39fb6e82495e468dbd96e0df74d0
travis1999 <trevoroguna@gmail.com>
2021-02-27 23:07:31 +0300

    added cache control

--- app.py
+++ app.py
@@ -9,5 +9,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True)
